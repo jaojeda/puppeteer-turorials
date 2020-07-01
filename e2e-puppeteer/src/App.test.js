53951e21f93ab8b22e2f51ff0faf755bff81720c
Jose <j.a.ojeda@live.com>
2020-06-30 20:32:52 -0700

    set up puppeteer test for boilerplate code

--- e2e-puppeteer/src/App.test.js
+++ e2e-puppeteer/src/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import puppeteer from "puppeteer";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let browser;
+let page;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/");
+});
+
+test("renders learn react link", async () => {
+  await page.waitForSelector(".App");
+
+  const header = await page.$eval(".App-header>p", e => e.innerHTML);
+  expect(header).toBe(`Edit <code>src/App.js</code> and save to reload.`);
+
+  const link = await page.$eval(".App-header>a", e => {
+    return {
+      innerHTML: e.innerHTML,
+      href: e.href
+    };
+  });
+  expect(link.innerHTML).toBe(`Learn React`);
+  expect(link.href).toBe("https://reactjs.org/");
+});
+
+afterAll(() => {
+  browser.close();
 });
